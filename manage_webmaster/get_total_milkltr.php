300948e65f18f80a9a98eb8f39b546614a22d73d
Srinivaslancius <srinivas@lanciussolutions.com>
2017-10-18 11:18:49 +0530

    remove unwanted pages

--- manage_webmaster/get_total_milkltr.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('admin_includes/config.php');
-$user_id =  $_POST['user_id'];
-$sql = "SELECT total_ltr FROM milk_orders WHERE user_id = '$user_id' ";
-$result = $conn->query($sql);
-$val=  $result->fetch_assoc();
-echo $val['total_ltr'];
-?>
